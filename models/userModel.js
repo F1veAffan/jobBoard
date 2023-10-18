faaaea55d321a5b518d8fede9b728296e36546ee
Affan <offarbase@gmail.com>
2023-10-18 05:33:27 -0700

    Done

--- models/userModel.js
+++ models/userModel.js
@@ -9,24 +9,20 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  u_additional: {
-    type: Object,
-    default: {full_name: {
-      type: String,
-      default: "",
-    },
-    address: {
-      type: String,
-      default: "",
-    },
-    phone_no: {
-      type: String,
-      default: "",
-    }}
+  full_name: {
+    type: String,
+    default: "",
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  phone_no: {
+    type: String,
+    default: "",
   },
 });
 
 const UserModel = mongoose.model("Users", UserSchema);
 
 module.exports = UserModel;
-
